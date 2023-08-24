8827a2bfbba0536eefc465edfca1036d027a9900
CatalinAndreii <118813905+CatalinAndreii@users.noreply.github.com>
2023-08-24 22:13:32 +0300

    Add files via upload

--- encode message.py
+++ encode message.py
@@ -1,33 +1,30 @@
+import random
+import string
 
-ALFA = ["a", "b", "c", "d", "e", "f", "g", "h", "i",
-        "j", "k", "l", "m", "n", "o", "p", "q", "r",
-        "s", "t", "u", "w", "x", "y", "z"]
+chars = " " + string.punctuation + string.digits + string.ascii_letters
+chars = list(chars)
+key = chars.copy()
 
-ENCOD = ["ᔑ", "ʖ", "ᓵ", "↸", "ᒷ", "⎓", "⊣", "⍑", "╎",
-         "⋮", "ꖌ", "ꖎ", "ᒲ", "リ", "𝙹", "!¡", "ᑑ", "∷",
-         "ᓭ", "ℸ ̣",  "⚍", "∴", " ̇/", "||", "⨅"]
+random.shuffle(key)
 
+# ENCRYPT
+plain_text = input("Enter a message to encrypt: ")
+cipher_text = ""
 
-message = input("Type anything you want: ")
-message = [*message]
-count = 0
-for j in message:
-    print(f"=>>>>>{j}")
-    count += 0
-    while j != ALFA[count]:
-        if count >= 25:
-            count -= 1
-            print("--=--", count)
-        else:
-            count += 1
-            print("--_--", count)
-    if j == ALFA[count]:
-        print(j)
+for letter in plain_text:
+    index = chars.index(letter)
+    cipher_text += key[index]
 
+print(f"original message : {plain_text}")
+print(f"encrypted message: {cipher_text}")
 
-'''
-for i in range(25):
-    ALFA[i] = ENCOD[i]
-    print(ALFA[i])
+# DECRYPT
+cipher_text = input("Enter a message to encrypt: ")
+plain_text = ""
 
-'''
+for letter in cipher_text:
+    index = key.index(letter)
+    plain_text += chars[index]
+
+print(f"encrypted message: {cipher_text}")
+print(f"original message : {plain_text}")
